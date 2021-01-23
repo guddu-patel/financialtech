cca7431b4f5dae8a2e1164d2a00b7f308ea8c213
guddu <guddu.patel13@gmail.com>
2021-01-24 02:32:25 +0545

    dynamic data aded

--- src/app/boost-business/boost-business.component.ts
+++ src/app/boost-business/boost-business.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boost-business.component.scss']
 })
 export class BoostBusinessComponent implements OnInit {
-
+  features = [
+    {
+      title: 'Quick cash disbursement',
+      'description': 'Get terms loans that your business needs within 72 hrs',
+      img: '/assets/img/Get Paid Instantly.png'
+    },
+    {
+      title: 'Low-interest rate',
+      'description': 'Achieve your financial goals with an amazing interest rate starting at 13% per annum',
+      img: '/assets/img/Low interest rate.png'
+    },
+    {
+      title: 'Zero Paperwork',
+      'description': 'Get started instantly by submitting only your basic details & bank statements',
+      img: '/assets/img/Secure Payments.png'
+    },
+    {
+      title: 'Ace your business finances',
+      'description': 'Manage banking, accounting & everything in between, on one platform',
+      img: '/assets/img/freelancer_feature_icon_6@1.5x.png'
+    },
+    {
+      title: 'Loans to fight COVID-19',
+      'description': 'Zero EMI for first 3 months on Back-to-Business loans of upto 1 lakh',
+      img: '/assets/img/Covid.png'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/feature-card/feature-card.component.ts
+++ src/app/feature-card/feature-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feature-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feature-card.component.scss']
 })
 export class FeatureCardComponent implements OnInit {
-
+  @Input() data: any;
   constructor() { }
 
   ngOnInit(): void {
