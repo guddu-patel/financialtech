070fa8bf9d7b34249a5e39b82f7e024884355afe
guddu <guddu.patel13@gmail.com>
2021-01-24 14:09:59 +0545

    mobile responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   loanForm: FormGroup;
+  income = { min: 100000, max: 300000, current: 200000, percent: 0 }
+  expense = { min: 10000, max: 100000, current: 25000, percent: 0 }
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.loanForm = this.fb.group({
-      monthlyIncome: [158000],
-      monthlyExpense: [0],
+      monthlyIncome: [this.income.current, [Validators.min(this.income.min), Validators.max(this.income.max)]],
+      monthlyExpense: [this.expense.current, [Validators.min(this.expense.min), Validators.max(this.expense.max)]],
       repaymentTanure: [6],
       haveExistingLone: [{ value: true }],
       existingLoanAmount: []
     })
+    this.onRangeChange(this.income);
+    this.onRangeChange(this.expense);
+  }
+  onRangeChange(data) {
+    data.percent = Math.ceil(((data.current - data.min) * 100) / (data.max - data.min))
   }
 
 }
